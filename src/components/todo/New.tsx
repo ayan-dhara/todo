b24d48eae2a7e199a152093ecc6aa00ceff18103
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-10-14 22:03:07 +0530

    added template

--- src/components/todo/New.tsx
+++ src/components/todo/New.tsx
@@ -1,8 +1,13 @@
 import {useAppSelector} from "../../redux/hooks/appHooks";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const New = (props: any) => {
   const {addTodo} = useAppSelector(state => state.todo)
+  const [template, setTemplate] = useState({
+    title:"",
+    description: "",
+    due: ""
+  })
   const [title, setTitle] = useState("")
   const [description, setDescription] = useState("")
   const [due, setDue] = useState("")
@@ -13,20 +18,46 @@ const New = (props: any) => {
       due: new Date(due).getTime(),
       status: props.index
     })
-    setDescription("")
-    setTitle("")
-    setDue("")
+    setTitle(template.title)
+    setDescription(template.description)
+    setDue(template.due)
   }
-
+  useEffect(()=>{
+    let savedTemplateString: any = localStorage.getItem("template:"+props.index)
+    if(savedTemplateString){
+      const savedTemplate = JSON.parse(savedTemplateString)
+      setTemplate(savedTemplate)
+      if(savedTemplate.title){
+        setTitle(savedTemplate.title)
+      }
+      if(savedTemplate.description){
+        setDescription(savedTemplate.description)
+      }
+      if(savedTemplate.due){
+        setDue(savedTemplate.due)
+      }
+      setTemplate(savedTemplate)
+    }
+  }, [])
+  const saveAsTemplate = () => {
+    const templateToSave = {
+      title,
+      description,
+      due
+    }
+    setTemplate(templateToSave)
+    localStorage.setItem("template:"+props.index, JSON.stringify(templateToSave))
+  }
   return (
     <>
       <div className="separator horizontal"/>
       <div className="add">
-        <input type="text" placeholder="Title" onChange={(e)=>setTitle(e.target.value)} value={title}/>
-        <textarea placeholder="Description" onChange={(e)=>setDescription(e.target.value)} value={description}/>
-        <input type="datetime-local" onChange={(e)=>setDue(e.target.value)} value={due}/>
+        <input type="text" placeholder="Title" onChange={(e) => setTitle(e.target.value)} value={title}/>
+        <textarea placeholder="Description" onChange={(e) => setDescription(e.target.value)} value={description}/>
+        <input type="datetime-local" onChange={(e) => setDue(e.target.value)} value={due}/>
         <div className="buttons">
           <button onClick={add}>Add</button>
+          <button onClick={saveAsTemplate}>Set as template</button>
           <button onClick={props.cancel}>Cancel</button>
         </div>
       </div>
