33e485ad43804a47b0b4c4254c4413ecb33be1e4
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-10-14 21:46:17 +0530

    Handled move to same list

--- src/components/todo/Item.tsx
+++ src/components/todo/Item.tsx
@@ -2,19 +2,23 @@ import {useAppDispatch, useAppSelector} from "../../redux/hooks/appHooks";
 import {setDragOver} from "../../redux/todo";
 
 const Item = (props: any) => {
-  const {dragoverIndex, updateTodo} = useAppSelector(state => state.todo)
+  const {todo: allTodo, dragoverIndex, updateTodo} = useAppSelector(state => state.todo)
   const {todo, todoId} = props
   const dispatch = useAppDispatch()
 
   const dragStop = (e: any) => {
+    dispatch(setDragOver(-1)) // will change async
     if(dragoverIndex < 0)
       return
-    console.log(todoId)
+    const selectedTodo: any = allTodo.todoList.filter((todo:any) => {
+      return todo.todoId === todoId
+    })
+    if(selectedTodo[0].status === dragoverIndex)
+      return;
     updateTodo({
       id: todoId,
       status: dragoverIndex
     })
-    dispatch(setDragOver(-1))
   }
   return (
     <div className="item" draggable={true} onDragEnd={dragStop}>
