ad457a21e2b6384352309865b27e96bcda87bd03
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-10-18 16:41:20 +0530

    Update delete.ts

--- api/todo/delete.ts
+++ api/todo/delete.ts
@@ -1,27 +1,29 @@
 // DELETE /todos/:id
-import {Request, Response} from "express";
+import {Response} from "express";
 import Todo from "../../models/todo";
 import list from "./list";
 
 // DELETE /api/todo/:id
-const deleteTodo = async (req:any, res: Response) => {
+const deleteTodo = async (req: any, res: Response) => {
   console.log("delete")
-  try{
+  try {
     const {id} = req.params
     const {userId} = req.user
-    const deleteData = await Todo.destroy({
+    const deleteData = await Todo.update({
+      deleted: true
+    }, {
       where: {
         todoId: id,
         userId
       }
     })
-    if(deleteData)
+    if (deleteData)
       return list(req, res)
     res.send({
       success: false,
       message: "Cannot delete"
     })
-  }catch (e){
+  } catch (e) {
     res.send({
       success: false,
       message: "Something went wrong while deleting"
